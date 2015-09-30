c4a77c249ad8f9c457e7bb425969770d3b1ac0c7
Eric Zhang <lighting_cattle@msn.cn>
2015-09-30 16:03:35 +0800

    login frame

---
+++ admincp/control/user.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/30 0030
+ * Time: 15:57
+ */
+class usercontrol extends adminbase
+{
+    function __construct()
+    {
+
+    }
+
+    public function onlogin()
+    {
+
+    }
+}
